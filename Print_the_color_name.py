ba871e5146c4bf52a4051fb9286c29eed2330443
srinidhiseethala <119839619+srinidhiseethala@users.noreply.github.com>
2023-05-08 12:20:02 +0530

    a=str(input()) if a=='V':     print("Violet") elif a=='I':     print("Indigo") elif a=='B':     print("Blue") elif a=='G':     print("Green") elif a=='Y':     print("Yellow") elif a=='O':     print("Orange") elif a=='R':     print("Red") else:     print(-1)

---
+++ Print_the_color_name.py
@@ -0,0 +1,17 @@
+a=str(input())
+if a=='V':
+    print("Violet")
+elif a=='I':
+    print("Indigo")
+elif a=='B':
+    print("Blue")
+elif a=='G':
+    print("Green")
+elif a=='Y':
+    print("Yellow")
+elif a=='O':
+    print("Orange")
+elif a=='R':
+    print("Red")
+else:
+    print(-1)
